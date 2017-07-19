a170ef1e2266e0f9adb25ab7f0c60cb5dd4247e6
sergio <slystopad@mirantis.com>
2017-07-19 12:53:08 +0300

    Fix bootstrap_reclass-model script

--- scripts/bootstrap_reclass-model.sh
+++ scripts/bootstrap_reclass-model.sh
@@ -9,11 +9,12 @@ RECLASS_HOME=${RECLASS_HOME:-'/srv/salt/reclass'}
 # generate model at https://10.10.100.8/ (lab777.tar.gz in the example)
 mkdir -p $RECLASS_HOME
 cd $RECLASS_HOME
-tar -xfz $CLUSTER_MODEL_ARC
+git init
+tar -xzf $CLUSTER_MODEL_ARC
 
 mkdir -p classes/cluster
-test ! -e .gitmodules || git submodule update --init --recursive
 git submodule add $SYSTEM_MODEL_GIT_URL classes/system/
+#test ! -e .gitmodules || git submodule update --init --recursive
 git add classes/system
 git commit -am "Initial commit cluster model $CLUSTER_MODEL_ARC and sys model $SYSTEM_MODEL_GIT_URL"
 
