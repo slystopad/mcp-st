f5c437024ebe90fc1b54eec7ff4f964082bd7510
sergio <slystopad@mirantis.com>
2017-07-19 13:11:11 +0300

    Fix bootstrap_reclass-model script

--- scripts/bootstrap_reclass-model.sh
+++ scripts/bootstrap_reclass-model.sh
@@ -7,6 +7,7 @@ source $0.env
 CLUSTER_MODEL_ARC=${CLUSTER_MODEL_ARC:-'~ubuntu/lab777.tar.gz'}
 SYSTEM_MODEL_GIT_URL=${SYSTEM_MODEL_GIT_URL:-'https://github.com/Mirantis/reclass-system-salt-model'}
 RECLASS_HOME=${RECLASS_HOME:-'/srv/salt/reclass'}
+test ! -e $CLUSTER_MODEL_ARC && { echo "Can't find reclass cluster model $CLUSTER_MODEL_ARC"; exit 1; }
 
 # generate model at https://10.10.100.8/ (lab777.tar.gz in the example)
 mkdir -p $RECLASS_HOME
